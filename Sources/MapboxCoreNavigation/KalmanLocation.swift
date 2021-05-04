ff56218d991c6a26e73b64063bf54485dc7cddb3
Wayne Abarquez <wayne.abarquez@navagis.com>
2021-05-04 16:02:38 +0800

    Fix errors on KalmanLocation.

--- Sources/MapboxCoreNavigation/KalmanLocation.swift
+++ Sources/MapboxCoreNavigation/KalmanLocation.swift
@@ -1,34 +1,33 @@
+import CoreLocation
+
 
 class KalmanLocation {
 
-    var decay: Double
+    var decay: Double = 3
 
-    var variance: Double
+    var variance: Double = -1
 
-    var minAccuracy: Double
+    var minAccuracy: Double = 1
 
-    var latitude: Double
+    var latitude: Double = 0
 
-    var longitude: Double
+    var longitude: Double = 0
 
-    var timestampInMs: Double
+    var timestampInMs: Double = 0
 
-    convenience init(decay: Double) {
-        self.decay = decay
-        self.variance = -1
-        self.minAccuracy = 1
+    convenience init(decay: Double = 3) {
+        self.init(decay: decay)
     }
 
     public func process(location: CLLocation) -> CLLocation {
-        var latitude = location.coordinate.latitude
-        var longitude = location.coordinate.longitude
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
         var accuracy = location.horizontalAccuracy
-        var timestampInMs = location.timestamp.timeIntervalSince1970
-
-        var newLocation = CLLocation(coordinate: location.coordinate, altitude: location.altitude, horizontalAccuracy: location.horizontalAccuracy, verticalAccuracy: location.verticalAccuracy, course: location.course, speed: location.speed, timestamp: location.timestamp)
-
-        if accuracy < self.minAccuracy
+        var timestampInMs = Double((location.timestamp.timeIntervalSince1970 * 1000.0).rounded())
+        
+        if accuracy < self.minAccuracy {
             accuracy = self.minAccuracy
+        }
 
         if self.variance < 0 {
           self.timestampInMs = timestampInMs
@@ -39,7 +38,7 @@ class KalmanLocation {
           let timeIncMs = timestampInMs - self.timestampInMs
 
           if timeIncMs > 0 {
-            self.variance += (timeIncMs * self.decay * self.decay) / 1000
+            self.variance += (timeIncMs * self.decay * self.decay) / 1000.0
             self.timestampInMs = timestampInMs
           }
 
@@ -50,8 +49,10 @@ class KalmanLocation {
           self.variance = (1 - _k) * self.variance
         }
 
-        newLocation.coordinate = CLLocationCoordinate2DMake(self.latitude, self.longitude)
+        let newCoordinate = CLLocationCoordinate2DMake(self.latitude, self.longitude)
 
+        let newLocation = CLLocation(coordinate: newCoordinate, altitude: location.altitude, horizontalAccuracy: location.horizontalAccuracy, verticalAccuracy: location.verticalAccuracy, course: location.course, speed: location.speed, timestamp: location.timestamp)
+        
         return newLocation
     }
 }
